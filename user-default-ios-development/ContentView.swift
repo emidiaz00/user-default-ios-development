d0ab531a8b52866227c0a8d1dda8e25b13e7976e
emildsn <emildsn00@gmail.com>
2023-06-21 15:43:25 -0300

    Initial Commit

---
+++ user-default-ios-development/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  user-default-ios-development
+//
+//  Created by Emiliano Diaz on 21/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ user-default-ios-development/user_default_ios_developmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  user_default_ios_developmentApp.swift
+//  user-default-ios-development
+//
+//  Created by Emiliano Diaz on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct user_default_ios_developmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
